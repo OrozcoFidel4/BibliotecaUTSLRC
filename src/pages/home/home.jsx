7668d14078bf2972fa49f059ad977c60c126d01a
OrozcoFidel4 <fidel21304022@gmail.com>
2025-06-20 17:36:47 -0700

    Home y tablas mejorados

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='font-semibold text-gray-900'>Home</div>
+    <div className='h-full w-full px-16'>
+    <div className="flex flex-col pt-6">
+       <div className="font-bold text-5xl mb-2">Inicio</div>
+        <div className="text-xl mb-12">Bienvenido, Jonh Doe</div>
+    </div>
+
+    <div className='w-full flex flex-row gap-4'>
+          <button className='h-64 flex-1 bg-[#88073f] text-gray-100 rounded-xl shadow-xl hover:bg-[#480422] items-'>Prestamos</button>
+          <button className='h-64 flex-1 bg-[#88073f] text-gray-100 rounded-xl shadow-xl hover:bg-[#480422] items-'>Prestamos</button>
+
+    </div>
+
+    </div>
   )
 }
 
--- src/pages/prestamos/prestamos.jsx
+++ src/pages/prestamos/prestamos.jsx
@@ -1,143 +1,155 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 function Prestamos() {
+  const [expanded, setExpanded] = useState(() => window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+
+    const handleResize = () => {
+      setExpanded(mediaQuery.matches);
+    };
+
+    // Set initial value and attach listener
+    handleResize();
+    mediaQuery.addEventListener("change", handleResize);
+
+    // Clean up on unmount
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
-    <div className='flex flex-col h-full w-full p-16'>
-    <div className='font-bold text-5xl mb-2'>
-      Préstamos
+    <div className="flex flex-col h-full w-full px-16 pt-6">
+      <div className="font-bold text-5xl mb-2">Préstamos</div>
+
+      <div className="text-xl mb-12">Listado de Préstamos en Activo</div>
+
+      <input
+        className={`bg-white w-72 px-4 py-2 mb-4 self-end rounded-lg shadow-md
+                           ${expanded ?  "h-auto" : "hidden" } overflow-hidden` }
+        type="text"
+        placeholder="Buscar"
+      />
+
+      <div className="h-full w-full">
+        <div className="overflow-hidden rounded-lg shadow-md mb-24">
+          <table className="min-w-full bg-white">
+            <thead>
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <th className="py-2 px-4 border-gray-400">ISBN</th>
+                <th className="py-2 px-4 border-gray-400">Título</th>
+                <th className="py-2 px-4 border-gray-400">Autor</th>
+                <th className="py-2 px-4 border-gray-400">Edición</th>
+                <th className="py-2 px-4 border-gray-400"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <td className="py-2 px-4 border-t border-gray-400 font-bold">
+                  9780875847405
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DESARROLLO HUMANO
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DIANE E. PAPALIA GABRIELA MARTORELL
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DECIMOTERCERA EDICIÓN
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400">
+                  <button className="h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]">
+                    Devolver
+                  </button>
+                </td>
+              </tr>
+
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <td className="py-2 px-4 border-t border-gray-400 font-bold">
+                  9780875847405
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DESARROLLO HUMANO
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DIANE E. PAPALIA GABRIELA MARTORELL
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DECIMOTERCERA EDICIÓN
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400">
+                  <button className="h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]">
+                    Devolver
+                  </button>
+                </td>
+              </tr>
+
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <td className="py-2 px-4 border-t border-gray-400 font-bold">
+                  9780875847405
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DESARROLLO HUMANO
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DIANE E. PAPALIA GABRIELA MARTORELL
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DECIMOTERCERA EDICIÓN
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400">
+                  <button className="h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]">
+                    Devolver
+                  </button>
+                </td>
+              </tr>
+
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <td className="py-2 px-4 border-t border-gray-400 font-bold">
+                  9780875847405
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DESARROLLO HUMANO
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DIANE E. PAPALIA GABRIELA MARTORELL
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DECIMOTERCERA EDICIÓN
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400">
+                  <button className="h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]">
+                    Devolver
+                  </button>
+                </td>
+              </tr>
+
+              <tr className="hover:bg-gray-100 cursor-pointer">
+                <td className="py-2 px-4 border-t border-gray-400 font-bold">
+                  9780875847405
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DESARROLLO HUMANO
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DIANE E. PAPALIA GABRIELA MARTORELL
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400 text-gray-500">
+                  DECIMOTERCERA EDICIÓN
+                </td>
+                <td className="py-2 px-4 border-t border-gray-400">
+                  <button className="h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]">
+                    Devolver
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-
-    <div className='text-xl mb-12'>
-      Listado de Préstamos en Activo
-    </div>
-    <input
-         className='bg-gray-50 w-72 h-6 p-6 mb-4 self-end rounded-lg shadow-md'
-          type="text" 
-          placeholder='Buscar'
-    />
-    
-    <div className='h-full w-full'>
-
-    <div class="overflow-hidden rounded-lg shadow-md">
-      <table class="min-w-full bg-gray-50">
-        <thead>
-          <tr>
-            <th class="py-2 px-4 border-b">ISBN</th>
-            <th class="py-2 px-4 border-b">TItulo</th>
-            <th class="py-2 px-4 border-b">Autor</th>
-            <th class="py-2 px-4 border-b">Edicion</th>
-            <th class="py-2 px-4 border-b">     </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-            <td class="py-2 px-4 border-b">
-              <button className='h-8 w-24 mx-2 bg-[#88073f] text-gray-100 rounded-lg hover:bg-[#480422]'>Prestamo</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-
-          <tr>
-            <td class="py-2 px-4 border-b">9780875847405</td>
-            <td class="py-2 px-4 border-b">DESARROLLO HUMANO</td>
-            <td class="py-2 px-4 border-b">DIANE E. PAPALIA GABRIELA MARTORELL</td>
-            <td class="py-2 px-4 border-b">DECIMOTERCERA EDICIÓN</td>
-          </tr>
-           
-        </tbody>
-      </table>
-    </div>
-    
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Prestamos
+export default Prestamos;
